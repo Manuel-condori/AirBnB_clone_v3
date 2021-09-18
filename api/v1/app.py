31658498c436ce59717fee5751cfb6b90bfb03d6
rennleon <2829@holbertonschool.com>
2021-09-18 23:05:31 +0000

    Create a funtion to handle http errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,8 +2,9 @@
 """This module starts a flask app"""
 from os import getenv
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,6 +20,12 @@ def teardown(exception):
 def hello_world():
     """Returns a text when / route is requested"""
     return 'Hello HBNB!'
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == '__main__':
