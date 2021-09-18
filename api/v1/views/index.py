17660621b0bfc112186f23acba427c89f30e74fb
rennleon <2829@holbertonschool.com>
2021-09-18 22:31:08 +0000

    Create an endpoint to list the number of each object by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 """This module declares index routes"""
+import models
 from flask import jsonify
 from api.v1.views import app_views
+
+classes = {
+    "Amenity": 'amenities',
+    "City": 'cities',
+    "Place": 'places',
+    "Review": 'reviews',
+    "State": 'states',
+    "User": 'users'
+}
 
 
 @app_views.route('/status')
 def index():
     """Returns a JSON response"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def count():
+    """Returns a JSON response"""
+    data = {}
+    for cls, nick in classes.items():
+        data[nick] = models.storage.count(cls)
+    return jsonify(data)
