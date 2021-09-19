76adfe4bacbea493083e14d7e04679e3bc00bda1
rennleon <2829@holbertonschool.com>
2021-09-19 21:12:16 +0000

    Create a RestFul API for Amenity resource

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This module declares city routes"""
+import models
+from flask import jsonify, abort
+from flask import request as req
+from models.amenity import Amenity
+from api.v1.views import app_views
+
+
+@app_views.route('/places/<place_id>/amenities')
+def place_amenity_objects(place_id):
+    """Returns amenity objects as JSON response"""
+    place = models.storage.get('Place', place_id)
+    if place is None:
+        abort(404)
+
+    amenities = [obj.to_dict() for obj in place.amenities]
+    return jsonify(amenities)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST', 'DELETE'])
+def place_amenity_res(place_id, amenity_id):
+    """Returns a Amenity object as JSON response"""
+    place = models.storage.get('Place', place_id)
+    if place is None:
+        abort(404)
+    amenity = models.storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)
+
+    amenity_linked = False
+    for a in place.amenities:
+        if a.id == amenity.id:
+            amenity_linked = True
+            break
+    if not amenity_linked:
+        abort(404)
+
+    if req.method == 'DELETE':
+        if models.storage_t == 'db':
+            place.amenities.remove(amenity)
+        else:
+            place.amenity_ids.rewmove(amenity.id)
+        models.storage.save()
+        return jsonify({})
+
+    if req.method == 'POST':
+        if amenity_linked:
+            return jsonify(amenity.to_dict()), 200
+
+        if models.storage_t == 'db':
+            place.amenities.push(amenity)
+        else:
+            place.amenity_ids.push(amenity.id)
+        models.storage.save()
+        return jsonify(amenity.to_dict()), 201
